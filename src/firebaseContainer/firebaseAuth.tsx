5c2a5b1ec0ce00e68742876962e5899d588cd256
Han Byul <yahb9@naver.com>
2022-09-06 16:16:16 +0900

    Feat: firebase auth, db reSetting

--- src/firebaseContainer/firebaseAuth.tsx
+++ src/firebaseContainer/firebaseAuth.tsx
@@ -1,4 +1,4 @@
-import { firebaseApp } from 'firebase'
+import { firebaseApp } from 'firebaseContainer'
 import { getAuth } from 'firebase/auth'
 
 export const firebaseAuthService = getAuth(firebaseApp)
--- src/firebaseContainer/firebaseDB.tsx
+++ src/firebaseContainer/firebaseDB.tsx
@@ -1,4 +1,4 @@
-import { firebaseApp } from 'firebase'
+import { firebaseApp } from 'firebaseContainer'
 import { getFirestore } from 'firebase/firestore'
 
 export const firebaseDB = getFirestore(firebaseApp)
