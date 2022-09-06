7b445e8d9e0ebebf603915379917ce54af1b0e67
Han Byul <yahb9@naver.com>
2022-09-06 19:34:03 +0900

    Fix: 장르 및 연도 선택 페이지 함수 렌더부분 바깥으로 빼서 수정

--- src/routes/SoloGame/GenreSelectPage/index.tsx
+++ src/routes/SoloGame/GenreSelectPage/index.tsx
@@ -1,12 +1,33 @@
-import SelectButton from 'components/SelectPage/SelectButton'
+import { useSetRecoilState } from 'recoil'
+import { useNavigate } from 'react-router-dom'
+
+import { selectInfoAtom } from 'store/atom'
+import SelectButton from 'components/SelectButton'
 
 const genreInitialType = ['BALLAD', 'KPOP', 'HIPOP', 'POPSONG']
 
 const GenreSelectPage = () => {
-  return genreInitialType.map((genre, i) => {
+  const setSelectedInfo = useSetRecoilState(selectInfoAtom)
+  const navigate = useNavigate()
+
+  const genreButtonList = genreInitialType.map((genre, i) => {
     const genreKey = `genre-${i}`
-    return <SelectButton key={genreKey} buttonName={genre} type='genre' />
+
+    const handleGenreButtonClick = () => {
+      setSelectedInfo((prevSelect) => {
+        return { ...prevSelect, genre }
+      })
+      navigate('/soloGame/selectYear')
+    }
+    return <SelectButton key={genreKey} buttonName={genre} handleClick={handleGenreButtonClick} />
   })
+
+  return (
+    <>
+      <div>원하시는 장르를 선택해주세요</div>
+      {genreButtonList}
+    </>
+  )
 }
 
 export default GenreSelectPage
--- src/routes/SoloGame/YearSelectPage/index.tsx
+++ src/routes/SoloGame/YearSelectPage/index.tsx
@@ -1,12 +1,30 @@
-import SelectButton from 'components/SelectPage/SelectButton'
+import { useSetRecoilState } from 'recoil'
+
+import { selectInfoAtom } from 'store/atom'
+import SelectButton from 'components/SelectButton'
 
 const yearInitialType = ['1980', '1990', '2000', '2010', '2020']
 
 const YearSelectPage = () => {
-  return yearInitialType.map((year, i) => {
+  const setSelectedInfo = useSetRecoilState(selectInfoAtom)
+
+  const genreButtonList = yearInitialType.map((year, i) => {
     const yearKey = `year-${i}`
-    return <SelectButton key={yearKey} buttonName={year} type='year' />
+
+    const handleYearButtonClick = () => {
+      setSelectedInfo((prevSelect) => {
+        return { ...prevSelect, year }
+      })
+    }
+    return <SelectButton key={yearKey} buttonName={year} handleClick={handleYearButtonClick} />
   })
+
+  return (
+    <>
+      <div>원하시는 연도를 선택해주세요</div>
+      {genreButtonList}
+    </>
+  )
 }
 
 export default YearSelectPage
